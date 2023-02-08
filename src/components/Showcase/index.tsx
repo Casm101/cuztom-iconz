98679949020319772674e72740769c19a5162346
Christian Smith Mantas <casm202@gmail.com>
2023-02-08 12:13:44 +0100

    Update: showcase now generates an icon dynamically

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,16 +1,27 @@
+import { Icon } from "../Icon";
 import "./styles.scss";
 
-export const Showcase = ({  }) => {
+import CuztomIconz from "../../icons/optimized-icons";
+
+
+export const Showcase = ({ name }: any) => {
+
+	const copyToClipboard = (data: string) => navigator.clipboard.writeText(data.toString());
+
+	const svg = CuztomIconz[name];
+
 	return (
     <div className="showcase">
       <div className="showcase-container">
-        <div className="container-sample">X</div>
+				<div className="container-sample">
+					<Icon name={name} fontSize={2} />
+				</div>
         <div className="container-overlay">
-          <button>copy SVG</button>
-          <button>copy JSX</button>
+          <button onClick={() => copyToClipboard(svg)}>copy SVG</button>
+					<button onClick={() => copyToClipboard('jsx')}>copy JSX</button>
         </div>
       </div>
-      <p className="showcase-title">sample-icon</p>
+      <p className="showcase-title">{name}</p>
     </div>
   );
 }
