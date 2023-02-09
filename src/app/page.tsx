543de43eea250184bba76df46eda9f236e01ce65
Christian Smith Mantas <casm202@gmail.com>
2023-02-09 12:14:42 +0100

    Update: dynamically display all icons in library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,25 @@
-import { Library, Navigation, Showcase, SiteHeader } from "@/components";
+import { Icon, Library, Navigation, Showcase, SiteHeader } from "@/components";
+import CuztomIconz from "../icons/optimized-icons";
+
+const genIconShowcase = () => {
+	
+	const iconArray = [];
+	for (let icon in CuztomIconz) {
+		iconArray.push(<Showcase name={icon} />)
+	}
+	return iconArray;
+}
 
 export default function Home() {
+
+	const iconArray = genIconShowcase();
+
   return (
     <>
-      <SiteHeader />
+      <SiteHeader icons={iconArray} />
       <Navigation />
       <Library>
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
-        <Showcase />
+				{iconArray}
       </Library>
     </>
   );
